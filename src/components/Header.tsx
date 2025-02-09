f92723e1c5078cdd21dee3b86278b728e990258a
Priyabrata <priyabrata8558@gmail.com>
2025-02-09 17:21:59 +0530

    correct header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,23 +15,24 @@ export default function Header() {
     <header className="z-[999] relative">
       {/* Background Bar */}
       <motion.div
-        className="fixed top-6 left-1/2 h-[3.5rem] w-[32rem] rounded-full border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75 -translate-x-1/2"
+        className="fixed top-0 sm:top-6 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75 -translate-x-1/2"
         initial={{ y: -100, x: "-50%", opacity: 0 }}
         animate={{ y: 0, x: "-50%", opacity: 1 }}
       ></motion.div>
 
-      <nav className="fixed top-[1.75rem] left-1/2 -translate-x-1/2 flex items-center h-[3rem]">
-        <ul className="flex w-[28rem] justify-between text-[0.9rem] font-medium text-gray-500 sm:w-[28rem]">
+      {/* Navigation */}
+      <nav className="fixed top-[0.15rem] sm:top-[1.7rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:h-[initial] sm:py-0">
+        <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.85rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5 sm:text-[0.9rem]">
           {links.map((link) => (
             <motion.li
-              className="relative flex items-center justify-center"
+              className="h-3/4 flex items-center justify-center relative"
               key={link.hash}
               initial={{ y: -100, opacity: 0 }}
               animate={{ y: 0, opacity: 1 }}
             >
               <Link
                 className={clsx(
-                  "flex items-center justify-center px-4 py-2 rounded-full transition hover:text-gray-950 dark:text-gray-300 dark:hover:text-gray-300",
+                  "flex w-full items-center justify-center px-3 py-3 rounded-full hover:text-gray-950 transition dark:text-gray-300 dark:hover:text-gray-300",
                   {
                     "text-gray-950 dark:text-white":
                       activeSection === link.name,
@@ -49,7 +50,11 @@ export default function Header() {
                   <motion.span
                     className="bg-gray-100 rounded-full absolute inset-0 -z-10 dark:bg-gray-800"
                     layoutId="activeSection"
-                    transition={{ type: "spring", stiffness: 380, damping: 30 }}
+                    transition={{
+                      type: "spring",
+                      stiffness: 380,
+                      damping: 30,
+                    }}
                   ></motion.span>
                 )}
               </Link>
